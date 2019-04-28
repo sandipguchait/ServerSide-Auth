778789e87d00ddd27714218843ba58741ac12232
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-28 17:24:26 +0530

    adding user model

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength:6,
+        trim: true
+    },
+});
+
+const model = mongoose.model('User', userSchema);
+module.exports = { model };
