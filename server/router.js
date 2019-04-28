7d5311abe7a17fcda5b808b140b50dc73e00c50f
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-28 20:56:02 +0530

    making a router folder separately

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
-module.exports = function(app) {
-    app.get('/', (req, res)=>{
-        res.send(['hi', 'hello'])
-    });
+const Authentication = require('./controllers/authentication');
+
+module.exports = (app) => {
+   app.post('/signup', Authentication.signup);
 } 
