34f83f88c9e4fedbb7a36268a166159dc4450c05
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-01 21:47:30 +0530

    making a new Route for feature component

--- client/src/index.js
+++ client/src/index.js
@@ -4,21 +4,23 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import Header from './components/Header';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import SignUp from './components/auth/Signup';
-
+import Feature from './components/Feature';
 import Reducer from './reducers/auth';
+
+
+
 const Root = () => (
     <BrowserRouter>
         <App>
             <Route path="/" exact component={Welcome} />
         </App>
         <Route path="/signup" component={SignUp} />
+        <Route path="/feature" component={Feature} />
     </BrowserRouter>
 )
 
