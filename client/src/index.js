ece2758fa9f08e56581c2460c06493b2a467d9d9
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-01 21:32:12 +0530

    adding redux to the project

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import Header from './components/Header';
 import App from './components/App';
 import Welcome from './components/Welcome';
+import SignUp from './components/auth/Signup';
 
-
+import Reducer from './reducers/auth';
 const Root = () => (
     <BrowserRouter>
         <App>
             <Route path="/" exact component={Welcome} />
         </App>
+        <Route path="/signup" component={SignUp} />
     </BrowserRouter>
 )
 
-ReactDOM.render(<Root/>, document.getElementById('root'));
+const store = createStore(
+    Reducer, 
+    composeWithDevTools(
+        applyMiddleware(reduxThunk)
+    )
+);
+
+ReactDOM.render(
+<Provider store={store}>
+    <Root/>
+</Provider>, 
+document.getElementById('root'));
