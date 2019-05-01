59eec76c9d4ea95da351976d4afd85b77a91791a
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-01 12:51:19 +0530

    adding new route to check authorization

--- server/router.js
+++ server/router.js
@@ -1,5 +1,13 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+//Checking Authentication status before visiting aparticular route
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
+   app.get('/', requireAuth, (req, res) => {
+      res.send({ Working: 'Yes'});
+   });
    app.post('/signup', Authentication.signup);
 } 
