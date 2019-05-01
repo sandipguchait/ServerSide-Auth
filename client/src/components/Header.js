410d67c1b5cba2e716b80eb82cba9ac228d16cb7
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-01 23:45:14 +0530

    Showing Links conditionnaly with authenticated users

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 class Header extends Component {
+
+    renderLinks () {
+        if(this.props.authenticated) {
+          return (
+              <div>
+                <Link to="/signout">Sign Out</Link>
+                <Link to="/feature">Feature Page</Link>
+              </div>
+          )
+        } else {
+            return (
+                <div>
+                    <Link to="/signup">Sign Up</Link>
+                    <Link to="/signin">Sign In</Link>
+                </div>
+            )
+        }
+    }
 
     render() {
         return (
            <React.Fragment>
                 <Link to="/">Redux Auth</Link>
-                <Link to="/signup">Sign Up</Link>
-                <Link to="/signin">Sign In</Link>
-                <Link to="/signput">Sign Out</Link>
-                <Link to="/feature">Feature Page</Link>
+               {this.renderLinks()}
            </React.Fragment>
         );
     }
+};
+
+const mapStateToProps = state => {
+    return  {
+        authenticated: state.auth.authenticated
+    }
 }
 
-export default Header;
+export default connect(mapStateToProps)(Header);
